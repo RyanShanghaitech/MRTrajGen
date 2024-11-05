75281b28f4949efaf307552294fec89aa1340d72
Rui Luo <luorui2023@shanghaitech.edu.cn>
2024-11-06 04:41:04 +0800

    modify README.md

--- Example/Example_Spiral3D.py
+++ Example/Example_Spiral3D.py
@@ -1,17 +1,19 @@
 from numpy import *
 from matplotlib.pyplot import *
 from mpl_toolkits.mplot3d import *
-import os
+
 import mrtrjgen
 
 gamma = 42.5756e6 # UIH
-pi = arccos(-1)
 fov = 0.25
 numPix = 128
+uTht = 32
+uPhi = 32
+dt = 10e-6
 
-for idxTht in range(32):
-    for idxPhi in range(32):
-        arrKxyz, arrGxyz = mrtrjgen.genSpiral3DTypeA(100*gamma*fov/numPix, numPix, 2*pi*idxTht/32, 2*pi*idxPhi/32, 32, 32, 2.5e-6, 0.5)
+for idxTht in range(uTht):
+    for idxPhi in range(uPhi):
+        arrKxyz, arrGxyz = mrtrjgen.genSpiral3DTypeB(100*gamma*fov/numPix, numPix, 2*pi*idxTht/uTht, 2*pi*idxPhi/uPhi, uTht, uPhi, dt, 0.5)
         save(f"../Resource/arrKxyz_tht{idxTht:02d}_phi{idxPhi:02d}.npy", arrKxyz)
 
 figure()
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Trajectory Generator for Magnetic Resonance Imaging
 
-Please cd to Example before you run them.
-Please cd to root directory before you run install.bash.
+Please cd to `Example` when you run `Example_*.py`
+Please cd to `root directory` when you run `install.bash`
