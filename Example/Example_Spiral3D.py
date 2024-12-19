ae9bcc69fe891ff028d9a3b5159e6f87a6d6ab78
Rui Luo <luorui2023@shanghaitech.edu.cn>
2024-12-19 10:03:46 +0800

    minimal modification to Example

--- Example/Example_Spiral3D.py
+++ Example/Example_Spiral3D.py
@@ -11,10 +11,10 @@ genSpiral3D = mrtrjgen.genSpiral3DTypeA # proposed Spiral-3D
 sr = 100 # desired slew rate
 fov = 0.5
 nPix = 256
-nTht = 96
+nTht = 64
 nPhi = 64
 dtGrad = 10e-6
-dtADC = 2.5e-6
+dtADC = 2e-6
 gamma = 42.5756e6 # UIH
 
 # calculate trajectory
@@ -24,7 +24,9 @@ lstArrSR = []
 scale = 1/gamma*nPix/fov
 for iPhi in range(nPhi):
     for iTht in range(nTht):
-        arrK, arrG = genSpiral3D(nPix, nTht, 2*nPhi, (2*pi)*(iTht/nTht), (2*pi)*(iPhi/nPhi), 0.5, sr*gamma*fov/nPix, dtGrad)
+        tht0 = (2*pi)*(iTht/nTht) #+ (2*pi/nTht)*(iPhi/nPhi)
+        phi0 = (2*pi)*(iPhi/nPhi)
+        arrK, arrG = genSpiral3D(nPix, nTht, nPhi, tht0, phi0, 0.5, sr*gamma*fov/nPix, dtGrad)
         # arrK, _ = mrtrjgen.intpTraj(arrG, dtGrad, dtADC)
         arrSR = (arrG[1:,:] - arrG[:-1,:])/dtGrad
         lstArrK.append(arrK)
@@ -32,12 +34,6 @@ for iPhi in range(nPhi):
         lstArrSR.append(asarray(sqrt((arrSR**2).sum(axis=-1)))*scale)
 
 # plot
-figure()
-for iTR in range(nPhi):
-    arrRho = asarray(lstArrK[iTR])
-    arrRho = sqrt(sum(arrRho**2, axis=-1))
-    plot(arrRho, ".-")
-
 markersize = 2
 linewidth = 1
 fig = figure(figsize=(14,8), dpi=150)
@@ -50,33 +46,38 @@ axSR = fig.add_subplot(gs[3:4,4:7])
 
 subplots_adjust(left=0.0, right=0.9, top=0.9, bottom=0.1, hspace=0.4, wspace=0.4)
 
-while 1:
-    for iTR in range(nPhi*nTht):
-        axK.clear()
-        axGx.clear()
-        axGy.clear()
-        axGz.clear()
-        axSR.clear()
+for iTR in range(len(lstArrK)):
+    axK.clear()
+    axGx.clear()
+    axGy.clear()
+    axGz.clear()
+    axSR.clear()
 
-        axK.plot(lstArrK[iTR][:,0], lstArrK[iTR][:,1], lstArrK[iTR][:,2], ".-", markersize=markersize, linewidth=linewidth)
-        axGx.plot(lstArrG[iTR][:,0], ".-", markersize=markersize, linewidth=linewidth)
-        axGy.plot(lstArrG[iTR][:,1], ".-", markersize=markersize, linewidth=linewidth)
-        axGz.plot(lstArrG[iTR][:,2], ".-", markersize=markersize, linewidth=linewidth)
-        axSR.plot(lstArrSR[iTR][:], ".-", markersize=markersize, linewidth=linewidth)
+    axK.plot(lstArrK[iTR][:,0], lstArrK[iTR][:,1], lstArrK[iTR][:,2], ".-", markersize=markersize, linewidth=linewidth)
+    axGx.plot(lstArrG[iTR][:,0], ".-", markersize=markersize, linewidth=linewidth)
+    axGy.plot(lstArrG[iTR][:,1], ".-", markersize=markersize, linewidth=linewidth)
+    axGz.plot(lstArrG[iTR][:,2], ".-", markersize=markersize, linewidth=linewidth)
+    axSR.plot(lstArrSR[iTR][:], ".-", markersize=markersize, linewidth=linewidth)
 
-        axK.set_xlim([-0.5,0.5])
-        axK.set_ylim([-0.5,0.5])
-        axK.set_zlim([-0.5,0.5])
-        axGx.set_ylim([-40e-3,40e-3])
-        axGy.set_ylim([-40e-3,40e-3])
-        axGz.set_ylim([-40e-3,40e-3])
-        axSR.set_ylim([0,2*sr])
+    axK.set_xlim([-0.5,0.5])
+    axK.set_ylim([-0.5,0.5])
+    axK.set_zlim([-0.5,0.5])
+    axGx.set_ylim([-40e-3,40e-3])
+    axGy.set_ylim([-40e-3,40e-3])
+    axGz.set_ylim([-40e-3,40e-3])
+    axSR.set_ylim([0,2*sr])
 
-        axK.set_title("K (/pix)")
-        axGx.set_title("Gx (T/m)")
-        axGy.set_title("Gy (T/m)")
-        axGz.set_title("Gz (T/m)")
-        axSR.set_title("Slew Rate (T/m/s)")
+    axK.set_title("K (/pix)")
+    axGx.set_title("Gx (T/m)")
+    axGy.set_title("Gy (T/m)")
+    axGz.set_title("Gz (T/m)")
+    axSR.set_title("Slew Rate (T/m/s)")
 
-        show(block=0)
-        pause(1e-6)
+    show(block=0)
+    pause(1e-3)
+
+# figure()
+# for iTR in range(len(lstArrK)):
+#     arrRho = asarray(lstArrK[iTR])
+#     arrRho = sqrt(sum(arrRho**2, axis=-1))
+#     plot(arrRho, ".-")
